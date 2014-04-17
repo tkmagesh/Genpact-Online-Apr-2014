a43f87d502658c3bd447ec640db8b23cf9797f11
tkmagesh <tkmagesh77@gmail.com>
2014-04-17 21:43:15 +0530

    Closures Demo & Assignment

---
+++ Day-1/Closures/getSpinner.js
@@ -0,0 +1,19 @@
+function getSpinner(){
+	//write the implemetation of the function here
+}
+
+var spinner = getSpinner();
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+spinner.up() // => 5
+
+spinner.down() // => 4
+spinner.down() // => 3
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
+
+
